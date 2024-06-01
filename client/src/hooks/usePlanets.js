28c5c381d67b6c859552ce5795db7de36c8d6d2e
Faraz Khan <farazkhanaa2555@gmail.com>
2024-06-01 20:43:56 +0500

    feat: create nasa project

--- client/src/hooks/usePlanets.js
+++ client/src/hooks/usePlanets.js
@@ -3,10 +3,11 @@ import { useCallback, useEffect, useState } from "react";
 import { httpGetPlanets } from "./requests";
 
 function usePlanets() {
-  const [planets, savePlanets] = useState([]);
+  const [planets, savePlanets] = useState([1]);
 
   const getPlanets = useCallback(async () => {
     const fetchedPlanets = await httpGetPlanets();
+    console.log("fetchplanets is : " , fetchedPlanets)
     savePlanets(fetchedPlanets);
   }, []);
 
