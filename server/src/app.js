523cb1e26ddcc56751208b6a3f70072f0dd24634
Faraz Khan <farazkhanaa2555@gmail.com>
2024-06-04 11:45:04 +0500

    feat: implement morgan as well as set build command

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const planetRouter = require("./routes/planets/planets.router");
 
 const app = express();
@@ -8,8 +10,15 @@ app.use(
     origin: "http://localhost:3006",
   })
 );
+
+app.use(morgan("combined"));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
