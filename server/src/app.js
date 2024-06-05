3ca566e0bb86a9745dc9ee7b29d868f6d40e4369
Faraz Khan <farazkhanaa2555@gmail.com>
2024-06-05 07:52:23 +0500

    feat: implement launches router in my project

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const planetRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 const app = express();
 app.use(
@@ -11,11 +12,12 @@ app.use(
   })
 );
 
-app.use(morgan("combined"));
+app.use(morgan("short"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetRouter);
+app.use(launchesRouter);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
