6e3abac373cc78e196c4b1a17f61f5a172b68e35
Faraz Khan <farazkhanaa2555@gmail.com>
2024-06-06 11:40:10 +0500

    feat: implement launches post request

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,13 +1,13 @@
 const launches = new Map();
 
-const latestFlightNumber = 100;
+let latestFlightNumber = 100;
 
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
-  destination: "kepler-442 b",
+  target: "kepler-442 b",
   customer: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -5,9 +5,25 @@ function httpGetAllLaunches(req, res) {
 }
 
 function httpAddNewLaunch(req, res) {
-  const launch = req.body;
+  let launch = req.body;
 
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch porperty",
+    });
+  }
   launch.launchDate = new Date(launch.launchDate);
+
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch Date",
+    });
+  }
 
   addNewLaunch(launch);
   return res.status(201).json(launch);
