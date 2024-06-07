4bb2893403ce8711eff1e2314df4ff6fa4f9b8e9
Faraz Khan <farazkhanaa2555@gmail.com>
2024-06-07 11:54:24 +0500

    feat: implement basic jest setup as well as write basic test for launches route

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,16 @@
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Test Get /launches", () => {
+  test("It Should respond with 200 success", async () => {
+    const response = await request(app).get("/launches").expect('Content-Type' , /json/).expect(200);
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("Test Post /launch", () => {
+  test("It should respond with 200 success", () => {});
+
+  test("It should catch missing required properties", () => {});
+  test("It should catch invalid dates", () => {});
+});
